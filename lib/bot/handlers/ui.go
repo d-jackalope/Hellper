30ccef97d9c116f2358711dccb6623978b85f88f
d-jackalope <di.jackalope@gmail.com>
2025-04-11 16:21:49 +0300

    Admin keyboards have been added.

--- lib/bot/handlers/ui.go
+++ lib/bot/handlers/ui.go
@@ -52,6 +52,45 @@ func renderAIServicesInlineKeyboard() models.InlineKeyboardMarkup {
 	}
 }
 
+func renderAdminPanelInlineKeyboard() models.InlineKeyboardMarkup {
+	return models.InlineKeyboardMarkup{
+		InlineKeyboard: [][]models.InlineKeyboardButton{
+			{
+				{Text: "Settings groups", CallbackData: strconv.Itoa(database.AuthMethodLocalAI)},
+				{Text: "Other", CallbackData: strconv.Itoa(database.AuthMethodOpenAI)},
+			},
+		},
+	}
+}
+
+func renderAdminSettingsGroupInlineKeyboard(groupList []string) models.InlineKeyboardMarkup {
+	buttons := [][]models.InlineKeyboardButton{}
+	for _, group := range groupList {
+		buttons = append(buttons, []models.InlineKeyboardButton{
+			{
+				Text:         group,
+				CallbackData: group,
+			},
+		})
+	}
+
+	return models.InlineKeyboardMarkup{
+		InlineKeyboard: buttons,
+	}
+}
+
+func renderAdminGroupInlineKeyboard() models.InlineKeyboardMarkup {
+	return models.InlineKeyboardMarkup{
+		InlineKeyboard: [][]models.InlineKeyboardButton{
+			{
+				{Text: "New token for this group", CallbackData: strconv.Itoa(database.AuthMethodLocalAI)},
+				{Text: "Change the Swarmind server", CallbackData: strconv.Itoa(database.AuthMethodLocalAI)},
+				{Text: "Change the bot's base AI model.", CallbackData: strconv.Itoa(database.AuthMethodLocalAI)},
+			},
+		},
+	}
+}
+
 // Render Language menu with Inline Keyboard
 // func renderLangInlineKeyboard() models.InlineKeyboardMarkup {
 // 	return models.InlineKeyboardMarkup{
