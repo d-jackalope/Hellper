e8e6d31b03898a909c1b60ddf47231fb1c4e36b2
d-jackalope <di.jackalope@gmail.com>
2025-04-11 16:22:47 +0300

    A wrapper for admin authorization has been implemented.

---
+++ lib/bot/handlers/adminPanel.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"github.com/JackBekket/hellper/lib/database"
+	"github.com/go-telegram/bot"
+)
+
+func adminАuthentication(user *database.User, tgb *bot.Bot) bool {
+
+	return false
+}
